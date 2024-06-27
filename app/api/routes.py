11a9cb7df878377d5586fdbaf756c4e1fa86ea08
Aram Martinez <amart122@asu.edu>
2024-06-26 23:29:09 -0700

    Finished basic recipe scraper using recipe_sracper and NLTK lemmatizer

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from .scraper import RecipeOrganizerImporter
 
 api_blueprint = Blueprint('api', __name__)
 
@@ -6,31 +7,8 @@ api_blueprint = Blueprint('api', __name__)
 def import_recipe():
   url = request.args.get('url')
   if not url:
-    return jsonify({"error": "URL is required"}), 400
+    return jsonify({"error": "URL is required"}), 403
   
   # IMPORT RECIPE
-  print("URL: " + url)
-  
-  return jsonify({
-    "recipe": {
-      "id": 0,
-      "name": "",
-      "aliases": [""],
-      "ingredients": [
-        {
-          "id": 1,
-          "quantity": 1,
-          "unit": "cup"
-        }
-      ],
-      "prepTime": 2,
-      "servingSize": 0,
-      "instructions": "xx",
-      "notes": "xx",
-      "description": "xx"
-    },
-    "ingredients": [{
-      "id:": 1,
-      "name": "XXX",
-    }]
-  })
+  recipe = RecipeOrganizerImporter().scrape_recipe(url)
+  return jsonify(recipe)
--- app/api/scraper.py
+++ app/api/scraper.py
@@ -1,90 +1,125 @@
+import math
+import random
+import nltk
+import json
+from nltk.stem import WordNetLemmatizer
+from flask import url_for
 from recipe_scrapers import scrape_me
 from recipe_scrapers._exceptions import WebsiteNotImplementedError
 from quantulum3 import parser
-import math
-import random
-import pdb
 
 class RecipeOrganizerImporter:
-  @staticmethod
-  def scrape_recipe(url):
+  def __init__(self):
+    single_word_ingredients, multi_word_ingredients = RecipeOrganizerImporter.load_ingredients()
+    self.single_word_ingredients = single_word_ingredients
+    self.multi_word_ingredients = multi_word_ingredients
+    self.lemmatizer = WordNetLemmatizer()
+
+  def scrape_recipe(self, url):
     try:
       scraper = scrape_me(url)
-      formatted_recipe = RecipeOrganizerImporter.format_recipe(scraper)
+      formatted_recipe = self.format_recipe(scraper)
 
       return formatted_recipe
     except WebsiteNotImplementedError:
       return {"error": "Website not implemented"}
 
-  @staticmethod
-  def format_recipe(scraper):
-    formatted_ingredient = RecipeOrganizerImporter.format_ingredients(scraper.ingredients())
+  def format_recipe(self, scraper):
+    formatted_ingredient = self.format_ingredients(scraper.ingredients())
     new_ingredients = [{"id": ingredient["id"], "name": ingredient["name"]} for ingredient in formatted_ingredient]
-
-    pdb.set_trace()
 
     formatted_recipe = {
       "id": RecipeOrganizerImporter.get_id(),
-      "name": recipe,
+      "name": scraper.title(),
       "ingredients": formatted_ingredient,
-      "instructions": scraper.instructions,
-      "prepTime": 0,
-      "servingSize": 0,
+      "instructions": scraper.instructions().split("\n"),
+      "prepTime": scraper.total_time(),
+      "servingSize": scraper.yields(),
       "notes": "",
-      "description": ""
+      "description": scraper.title(),
     }
 
-    return formatted_recipe, new_ingredients
+    return [formatted_recipe, new_ingredients]
 
-  @staticmethod
-  def format_ingredients(ingredients):
+  def format_ingredients(self, ingredients):
     formatted_ingredients = []
+
     for ingredient in ingredients:
-      parsed_ingredient = RecipeOrganizerImporter.parse_ingredient(ingredient)
+      name, raw = self.parse_ingredient_name(ingredient)
+      quantity, unit = self.parse_unit_quantity(ingredient)
       
       formatted_ingredient = {
         "id": RecipeOrganizerImporter.get_id(),
-        "name": parsed_ingredient['name'],
-        "quantity": parsed_ingredient['quantity'],
-        "unit": parsed_ingredient['unit'],
+        "name": name,
+        "quantity": quantity,
+        "unit": unit,
+        "raw": raw,
       }
       formatted_ingredients.append(formatted_ingredient)
 
     return formatted_ingredients
+
+  def parse_unit_quantity(self, ingredient):
+    quant = [quant for quant in parser.parse(ingredient) if quant.unit.name != "dimensionless"]
+    quant = [quant for quant in parser.parse(ingredient)] if len(quant) == 0 else quant
+    quantity = quant[0].value if len(quant) > 0 else ''
+    unit = quant[0].unit.name if len(quant) > 0 and quant[0].unit and quant[0].unit.name != "dimensionless" else ''
+
+    return (quantity, unit)
+
+  def parse_ingredient_name(self, ingredient):
+    ingredient_array = ingredient.split()
+    name = ''
+
+    for idx, word in enumerate(ingredient_array):
+      ingredient_name = None
+
+      try:
+        word1 = self.process_word(word)
+        word2 = self.process_word(ingredient_array[idx + 1]) if len(ingredient_array) > idx + 1 else None
+        word3 = self.process_word(ingredient_array[idx + 2]) if len(ingredient_array) > idx + 2 else None
+
+        if word3 and word1 + " " + word2 + " " + word3 in self.multi_word_ingredients:
+          ingredient_name = self.multi_word_ingredients[word1 + " " + word2 + " " + word3]
+        elif word2 and word1 + " " + word2 in self.multi_word_ingredients:
+          ingredient_name = self.multi_word_ingredients[word1 + " " + word2]
+        elif word1 in self.single_word_ingredients:
+          ingredient_name = word1
+      except AttributeError:
+        print("AttributeError: " + word1 + " " + word2 + " " + word3)
+        pass
+      except KeyError:
+        print("KeyError: " + word1 + " " + word2 + " " + word3)
+        pass
+
+      if ingredient_name:
+        name = ingredient_name
+        break
+
+    return (name, ingredient) if name else (ingredient, ingredient)
+
+  def process_word(self, word):
+    return self.lemmatizer.lemmatize(word.replace(",", "")
+              .replace(".", "")
+              .replace("(", "")
+              .replace(")", "")
+              .replace(":", "")
+              .replace(";", "")
+              .replace("!", "")
+              .replace("?", "")
+              .replace("'", "")
+              .replace('"', "")
+              .lower())
 
   @staticmethod
   def get_id():
     return  math.floor(random.random() * 1000000000)
 
   @staticmethod
-  def parse_ingredient(ingredient):
-    quant = [quant for quant in  parser.parse(ingredient) if quant.unit.name != "dimensionless"]
-    quantity = quant[0].value if len(quant) > 0 else ''
-    unit = quant[0].unit.name if len(quant) > 0 and quant[0].unit else ''
-    name = "xx"
+  def load_ingredients():
+    with open('app/static/single_word_ingredients.json') as f:
+      single_word_ingredients = json.load(f)
+    with open('app/static/multi_word_ingredients.json') as f:
+      multi_word_ingredients = json.load(f)
 
-    return {
-      "name": name,
-      "unit": unit,
-      "quantity": quantity,
-      "raw": ingredient
-    }
-
-
-    # pattern = re.compile(r'(\d*\.?\d+\/?\d*)?\s*(\w+)?\s*([^,]+)(?:,\s*(.*))?')
-    # match = pattern.match(ingredient)
-    
-    # if match:
-    #   quantity = match.group(1) if match.group(1) else ''
-    #   unit = match.group(2) if match.group(2) else ''
-    #   ingredient = match.group(3) if match.group(3) else ''
-    #   extra_info = match.group(4) if match.group(4) else ''
-
-    #   return {
-    #     "quantity": quantity.strip(),
-    #     "unit": unit.strip(),
-    #     "ingredient": ingredient.strip(),
-    #     "extra_info": extra_info.strip()
-    #   }
-    # else:
-    #   return None
+    return (single_word_ingredients, multi_word_ingredients)
