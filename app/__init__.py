636d15465a1d2fdc506c69ebea1fe3aa9c675945
Aram Martinez <amart122@asu.edu>
2024-07-22 18:12:00 -0700

    Setup app to use SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
   app.config.from_object(Config)
+  
+  db.init_app(app)
+  migrate = Migrate(app, db)
 
   from .api.routes import api_blueprint
+  from .api.ingredients import ingredients_blueprint
   app.register_blueprint(api_blueprint, url_prefix='/api')
+  app.register_blueprint(ingredients_blueprint, url_prefix='/api/storage')
 
   return app
--- app/config.py
+++ app/config.py
@@ -3,3 +3,5 @@ import os
 class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
   ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
