92bebb4d0b086f0fc4f98a02c1f12538d651a625
Aram Martinez <amart122@asu.edu>
2024-07-22 18:13:06 -0700

    Introduced Ingredients Model/Route

---
+++ app/api/ingredients.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, jsonify, request
+from app import db
+from flask import current_app
+
+ingredients_blueprint = Blueprint('ingredients', __name__)
+
+class Ingredient(db.Model):
+  __tablename__ = 'ingredients'  
+  id = db.Column(db.Integer, primary_key=True)
+  local_id = db.Column(db.String(64), index=True, unique=True)
+  name = db.Column(db.String(64), index=True, unique=True)
+  recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'))
+
+  def __repr__(self):
+    return f'<Ingredient {self.name}-{self.localId}>'
