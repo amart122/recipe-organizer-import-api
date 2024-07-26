ae046a8cdde4bbe6b50b11af813be9c16bfdf953
Aram Martinez <amart122@asu.edu>
2024-07-26 14:23:55 -0700

    removed uniqueness of name

--- app/api/ingredients.py
+++ app/api/ingredients.py
@@ -2,14 +2,13 @@ from flask import Blueprint, jsonify, request
 from app import db
 from flask import current_app
 from middleware import firebase_login
-
 ingredients_blueprint = Blueprint('ingredients', __name__)
 
 class Ingredient(db.Model):
   __tablename__ = 'ingredients'  
   id = db.Column(db.Integer, primary_key=True)
   local_id = db.Column(db.String(64), index=True, unique=True)
-  name = db.Column(db.String(64), index=True, unique=True)
+  name = db.Column(db.String(64), index=True)
   user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
   def __repr__(self):
